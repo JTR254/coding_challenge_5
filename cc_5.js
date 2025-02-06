cee914759322cc4c65178da524a3a860ac940c58
JTR254 <jackrindge@gmail.com>
2025-02-06 11:44:38 -0500

    Task 1 - Customer Profile

---
+++ cc_5.js
@@ -0,0 +1,11 @@
+// Task 1 - Customer Profile
+
+let customer = {
+    name: "Carmen Rindge",
+    age: 28,
+    email:"carmenrindge@gmail.com"
+}; // created an object with properties regarding the customer's information
+
+console.log ("Customer Profile") // creates a heading to the task named "Customer Prile"
+console.log (`Name: ${customer.name}, Age: ${customer.age}, Email: ${customer.email}`) // logged the customer's name, age, and email
+
