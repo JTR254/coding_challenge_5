5f8fd87edb0c8a543f8974ed98edc8cbd6a8a5bb
JTR254 <jackrindge@gmail.com>
2025-02-09 12:53:21 -0500

    Task 2 - Order Details; also deleted an extra console.log in Task 1

--- cc_5.js
+++ cc_5.js
@@ -6,6 +6,23 @@ let customer = {
     email:"carmenrindge@gmail.com"
 }; // created an object with properties regarding the customer's information
 
-console.log ("Customer Profile") // creates a heading to the task named "Customer Prile"
-console.log (`Name: ${customer.name}, Age: ${customer.age}, Email: ${customer.email}`) // logged the customer's name, age, and email
+console.log (`Name: ${customer.name}, Age: ${customer.age}, Email: ${customer.email}`); // logged the customer's name, age, and email
+
+// Task 2 - Order Details
+
+let order = { // creates an object
+    orderID: 14254,
+    totalAmount: 150,
+    status: "Processing",
+
+    displayOrder() {
+        console.log(`Order ID: ${this.orderID}`) // logs the order ID
+        console.log(`Total Amount: ${this.totalAmount}`) // logs the total amount
+        console.log(`Status: ${this.status}`) // logs the status of the order
+    }
+};
+
+order.displayOrder(); // calling the method, then logs the details to the console
+
+
 
